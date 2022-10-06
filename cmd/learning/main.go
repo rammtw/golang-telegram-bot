e1ee24905c3f069cab76cf9a3e32e7614d14ef58
Ramil <rammtw@gmail.com>
2022-10-06 18:25:58 +0300

    add telegram api lib

--- cmd/learning/main.go
+++ cmd/learning/main.go
@@ -4,11 +4,12 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
 	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello from CMD!")
+		fmt.Fprintf(w, "TOKEN: "+os.Getenv("TELEGRAM_TOKEN"))
 	})
 
 	if err := http.ListenAndServe(":8080", nil); err != nil {
